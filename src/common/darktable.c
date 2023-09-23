fcbb66a283b0f55e15387854d094fa263ddbaf71
Mario Zimmermann <mail@zisoft.de>
2023-09-23 15:43:02 +0200

    initialize metal devices in darktable.metal

--- src/common/darktable.c
+++ src/common/darktable.c
@@ -1559,7 +1559,8 @@ int dt_init(int argc, char *argv[], const gboolean init_gui, const gboolean load
     dt_opencl_init(darktable.opencl, exclude_opencl, print_statistics);
 
 #ifdef __APPLE__
-  dt_metal_init();
+  darktable.metal = (dt_metal_t *)calloc(1, sizeof(dt_metal_t));
+  dt_metal_init(darktable.metal);
 #endif
 
   darktable.points = (dt_points_t *)calloc(1, sizeof(dt_points_t));
